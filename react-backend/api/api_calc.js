472f328fa4108b72551e4b10ed7d5d19d507c2d8
Mokuten OH <mokuteno@gmail.com>
2018-03-18 02:28:16 -0700

    add api to handle add operation

---
+++ react-backend/api/api_calc.js
@@ -0,0 +1,44 @@
+
+module.exports.calcAdd = function (req, res, next) {
+
+	console.log(req.query);
+	var a = req.query.a;
+	var b = req.query.b;
+
+	var result = {};
+	if(!isNaN(a + '') || !isNaN(b + ''))
+	{	
+		result = {
+			result: "Not a number"
+		}
+		res.type('json');
+		res.send(JSON.stringify(result));
+		return;
+	}
+	a = parseFloat(a);
+	b = parseFloat(b);
+	
+	result = a + b;
+	res.type('json');
+	res.send(JSON.stringify({
+		result: result
+	}));
+
+	return;
+}
+
+
+module.exports.calcSub = function (req, res, next) {
+	
+	
+}
+
+module.exports.calcMul = function (req, res, next) {
+	
+	
+}
+
+module.exports.calcDiv = function (req, res, next) {
+	
+	
+}
