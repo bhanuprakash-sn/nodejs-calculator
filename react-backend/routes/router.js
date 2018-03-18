55834a4adfad614cea0d3a7fa3c5a01c7bb87a20
Mokuten OH <mokuteno@gmail.com>
2018-03-18 02:28:00 -0700

    rename controllers

--- react-backend/routes/router.js
+++ react-backend/routes/router.js
@@ -3,9 +3,9 @@ var path = require('path');
 var router = express.Router();
 var api_calc = require('../api/api_calc');
 
-router.get('/api_get_add_result', api_calc.getAdd);
-router.get('/api_get_sub_result', api_calc.getSub);
-router.get('/api_get_mul_result', api_calc.getMul);
-router.get('/api_get_div_result', api_calc.getDiv);
+router.get('/api_get_add_result', api_calc.calcAdd);
+router.get('/api_get_sub_result', api_calc.calcSub);
+router.get('/api_get_mul_result', api_calc.calcMul);
+router.get('/api_get_div_result', api_calc.calcDiv);
 
 module.exports = router;
